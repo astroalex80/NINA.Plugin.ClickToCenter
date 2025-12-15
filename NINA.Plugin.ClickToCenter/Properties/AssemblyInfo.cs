aa578143d9326a7cb1797229e88cd0a7f1657570
Alex <47869238+astroalex80@users.noreply.github.com>
2025-12-15 11:48:44 +0100

    update info

--- NINA.Plugin.ClickToCenter/Properties/AssemblyInfo.cs
+++ NINA.Plugin.ClickToCenter/Properties/AssemblyInfo.cs
@@ -31,18 +31,18 @@ using System.Runtime.InteropServices;
 // The url to the license
 [assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/en-US/MPL/2.0/")]
 // The repository where your pluggin is hosted
-[assembly: AssemblyMetadata("Repository", "to be added")]
+[assembly: AssemblyMetadata("Repository", "https://github.com/astroalex80/NINA.Plugin.ClickToCenter/")]
 
 // The following attributes are optional for the official manifest meta data
 
 //[Optional] Your plugin homepage URL - omit if not applicaple
-[assembly: AssemblyMetadata("Homepage", "to be added")]
+[assembly: AssemblyMetadata("Homepage", "https://github.com/astroalex80/NINA.Plugin.ClickToCenter/blob/master/README.md")]
 
 //[Optional] Common tags that quickly describe your plugin
 [assembly: AssemblyMetadata("Tags", "Centering, Plate-solving")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
-[assembly: AssemblyMetadata("ChangelogURL", "to be added/CHANGELOG.md")]
+[assembly: AssemblyMetadata("ChangelogURL", "")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
 [assembly: AssemblyMetadata("FeaturedImageURL", "https://github.com/astroalex80/NINA.Plugin.ClickToCenter/blob/master/NINA.Plugin.ClickToCenter/screenshot.PNG")]
@@ -51,8 +51,8 @@ using System.Runtime.InteropServices;
 //[Optional] An additional url to an example example screenshot of your plugin in action
 [assembly: AssemblyMetadata("AltScreenshotURL", "")]
 //[Optional] An in-depth description of your plugin
-[assembly: AssemblyMetadata("LongDescription", @"Select a target by right-clicking on the current image in Click to Center dockable and hit slew and center.
-When executed, the plugin plate-solves the current image, converts the selected pixel position to RA/Dec, and runs a centering routine to slew/center the mount.")]
+[assembly: AssemblyMetadata("LongDescription", @"Select a target by right-clicking on the current image in the Click to Center dockable and hit slew and center.
+The plugin plate-solves the current image, converts the selected pixel position to RA/Dec coordinates, and runs a centering routine to slew/center the mount on the .")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
